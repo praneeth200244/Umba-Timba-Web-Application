aa9a8b6b6064504fef516916669501ab843d6986
Praneeth Devadiga <praneethdevadiga44@gmail.com>
2024-05-08 14:28:57 +0530

    Forgot Password setup, Optimizing sending mail

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,7 @@ from accounts.forms import UserForm
 from accounts.models import User, UserProfile
 from django.contrib import messages, auth
 
-from accounts.utils import detectUser
+from accounts.utils import detectUser,send_verification_email
 from vendor.forms import VendorForm
 
 from django.contrib.auth.decorators import login_required,user_passes_test
@@ -42,9 +42,13 @@ def registerCustomer(request):
             user.set_password(password)
             user.role = User.CUSTOMER
             user.save()
-            send_verification_email(request, user)
+
+            # Send verification email
+            mail_subject = 'Please activate your account'
+            email_template = 'accounts/emails/account_verification_email.html'
+            send_verification_email(request, user, mail_subject, email_template)
             messages.success(request, "Your account has been registered successfully!. Please check your mail to activate your account")
-            return redirect('login')
+            return redirect('myAccount')
         else:
             messages.error(request, "Account registration unsuccessful")
             context = {
@@ -89,8 +93,13 @@ def registerVendor(request):
             vendor.user_profile = UserProfile.objects.get(user=user)
             vendor.save()
 
-            messages.success(request, "Your account has been registered successfully. Please wait for the approval")
-            return redirect('login')
+            # Send verification email
+            mail_subject = 'Please activate your account'
+            email_template = 'accounts/emails/account_verification_email.html'
+            send_verification_email(request, user, mail_subject, email_template)
+
+            messages.success(request, "Your account has been registered successfully. Please check your mail to activate your account and wait for approval")
+            return redirect('myAccount')
         else:
             print(user_form.errors)
             print(vendor_form.errors)
@@ -126,6 +135,60 @@ def activate(request, uidb64, token):
     else:
         messages.error(request, "Invalid activation link")
         return redirect('myAccount')
+
+def forgot_password(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+
+        if User.objects.filter(email=email).exists():
+            user = User.objects.get(email__exact=email)
+
+            # Send reset password email
+            mail_subject = 'Reset your password'
+            email_template = 'accounts/emails/reset_password_email.html'
+            send_verification_email(request, user, mail_subject, email_template)
+
+            messages.success(request, "Password reset link has been sent to your registered email address")
+            return redirect('myAccount')
+        else:
+            messages.error(request, "Account doesn't exists")
+            return redirect('forgot_password')
+        
+    return render(request, 'accounts/forgot_password.html')
+
+def reset_password_validate(request, uidb64, token):
+    # Validate the user
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = User._default_manager.get(pk=uid)
+    except(ValueError, OverflowError, TypeError, User.DoesNotExist):
+        user = None
+    
+    if user is not None and default_token_generator.check_token(user, token):
+        request.session['uid'] = uid
+        messages.info(request, "Please reset your password")
+        return redirect('reset_password')
+    else:
+        messages.error(request, 'This link has been expired')
+        return redirect('myAccount')
+
+def reset_password(request):
+    if request.method == 'POST':
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            pk = request.session.get('uid')
+            user = User.objects.get(pk=pk)
+            user.set_password(password)
+            user.is_active = True
+            user.save()
+            messages.success(request, "Passwords updated successfully. Please login to your account")
+            return redirect('myAccount')
+        else:
+            messages.error(request, "Passwords do not match!")
+            return redirect('reset_password')
+    return render(request, 'accounts/reset_password.html')
 
 def login(request):
     if request.user.is_authenticated:
